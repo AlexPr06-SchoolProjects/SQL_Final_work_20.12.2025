8121271486cc68122fb7ab1a48eaaa32e6353223
AlexPr06 <freezay00828002@gmail.com>
2026-01-18 15:00:10 +0200

    First exam assignment done

--- SQL-final-work-20.12.2025.sql
+++ SQL-final-work-20.12.2025.sql
@@ -1,9 +1,9 @@
+
+USE p41_mystat_db;	
+
 -- Итоговое задание (Вариант 1)
 	--! 1. Добавить возможность того, что студент может состоять только в одной учебной группе
 
-
-USE p41_mystat_db;	
-	-- Option 1
 	ALTER TABLE student_profiles
 	ADD group_id int NOT NULL;
 
@@ -11,28 +11,14 @@ USE p41_mystat_db;
 	ADD CONSTRAINT FK_student_profiles_group 
 	FOREIGN KEY (group_id) REFERENCES groups(id);
 
-
-	-- Option 2 if some extra logic will be required
-	CREATE TABLE groups_students (
-		group_id int NOT NULL,
-		student_id int NOT NULL,
-
-		CONSTRAINT PK_groups_students PRIMARY KEY (group_id, student_id),
-		CONSTRAINT FK_groups_students_group FOREIGN KEY (group_id) REFERENCES groups (id),
-		CONSTRAINT FK_groups_students_pair FOREIGN KEY (student_id) REFERENCES student_profiles (id)
-	);
+--#############################################################################################################################################################################
 
 	-- 2. Добавить возможность определять тему каждой пары
 
-	CREATE TABLE subjects_topics (
+	CREATE TABLE topics (
 		id int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
 		title nvarchar(128) NOT NULL,
 		info nvarchar(256) NOT NULL,
-		
-		-- Тема зависит от предмета, который в свою очередь зависит от flow
-		subject_id int NOT NULL,
-
-		CONSTRAINT FK_themes_flow FOREIGN KEY (subject_id) REFERENCES subjects(id)
 	);
 
 
@@ -41,32 +27,12 @@ USE p41_mystat_db;
 
 
 	ALTER TABLE pairs 
-	ADD CONSTRAINT FK_pairs_topic FOREIGN KEY (topic_id) REFERENCES subjects_topics(id);
+	ADD CONSTRAINT FK_pairs_topic FOREIGN KEY (topic_id) REFERENCES topics(id);
 
-	-- Конечно, логику соответствия между pairs.subject_id и subjects_topics.subject_id 
-	-- можно реализовать у клиента, но я все-таки потренеируюсь и реализую логику простенького триггера
-	CREATE TRIGGER trg_pairs_topic_check
-	ON pairs
-	AFTER INSERT, UPDATE
-	AS
-	BEGIN
-		SET NOCOUNT ON; -- чтобы не было лишних сообщений
-
-		IF EXISTS (
-			SELECT 1
-			FROM inserted i
-			JOIN subjects_topics t ON i.topic_id = t.id
-			WHERE i.subject_id <> t.subject_id
-		)
-		BEGIN
-			RAISERROR('Topic does not match the subject!', 16, 1);
-			ROLLBACK TRANSACTION;
-		END
-	END;
-
+--#############################################################################################################################################################################
 
 	-- 3. Добавить возможность на паре:
-    --3.1. Отмечать статус студента (отсутствует, опоздал, присутствует)
+    -- 3.1. Отмечать статус студента (отсутствует, опоздал, присутствует)
 		CREATE TABLE student_presence_status (
 			id tinyint PRIMARY KEY IDENTITY(1, 1),
 			title nvarchar(128) NOT NULL,
@@ -95,24 +61,21 @@ USE p41_mystat_db;
 			CONSTRAINT FK_class_register_notes_student FOREIGN KEY (student_id) REFERENCES student_profiles (id),
 			CONSTRAINT FK_class_register_notes_status FOREIGN KEY (status_id) REFERENCES student_presence_status (id),
 			CONSTRAINT FK_class_register FOREIGN KEY (class_register_id) REFERENCES class_registers (id),
-
-			-- один студент = одна запись в журнале (если нужна такая логика)
-			-- CONSTRAINT UQ_register_student UNIQUE (class_register_id, student_id)
 		);
 
 
 		-- Логика автоматического создания журнала может быть имплементирована
 		-- в бэкенде. Ради практики сделаю через тригер
-		CREATE TRIGGER trg_create_class_register
-		ON pairs
-		AFTER INSERT
-		AS
-		BEGIN
-			SET NOCOUNT ON;
-			INSERT INTO class_registers (pair_id)
-			SELECT id 
-			FROM inserted;
-		END;
+		--CREATE TRIGGER trg_create_class_register
+		--ON pairs
+		--AFTER INSERT
+		--AS
+		--BEGIN
+		--	SET NOCOUNT ON;
+		--	INSERT INTO class_registers (pair_id)
+		--	SELECT id 
+		--	FROM inserted;
+		--END;
 
 
 
@@ -125,6 +88,10 @@ USE p41_mystat_db;
 
 		CONSTRAINT CH_grades_grade CHECK (1 <= grade AND grade <= 12)
 	);
+
+	-- Заполнение таблички grades оценками
+	INSERT INTO grades (grade) VALUES 
+	(1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12);
 
 
 	ALTER TABLE class_register_notes
@@ -149,42 +116,86 @@ USE p41_mystat_db;
 		CONSTRAINT FK_comments_user FOREIGN KEY (user_id) REFERENCES users (id)
 	);
 
-
-	-- Старая логика
-	-- #################################################################################################################
-	--! Возможность добавдения коментария учителем
-	--ALTER TABLE class_register_notes
-	---- комента может не быть, поэтому NULL
-	--ADD teacher_comment_id int NULL;
-
-	--ALTER TABLE class_register_notes
-	--ADD CONSTRAINT FK_class_register_notes_teacher_comment  FOREIGN KEY (teacher_comment_id) REFERENCES comments (id);
-
-	----! Возможность добавдения коментария учеником
-	--ALTER TABLE class_register_notes
-	---- комента может не быть, поэтому NULL
-	--ADD student_comment_id int NULL;
-
-	--ALTER TABLE class_register_notes
-	--ADD CONSTRAINT FK_class_register_notes_student_comment  FOREIGN KEY (student_comment_id) REFERENCES comments (id);
-	-- #################################################################################################################
-
-	-- Новая логика
-	--#################################################################################################################
-
-	ALTER TABLE class_register_notes
-	DROP CONSTRAINT FK_class_register_notes_student_comment, FK_class_register_notes_teacher_comment;
-
-	ALTER TABLE class_register_notes
-	DROP COLUMN teacher_comment_id, student_comment_id;
-
+	
 	CREATE TABLE class_register_notes_comments (
+		id int PRIMARY KEY IDENTITY(1, 1),
 		comment_id int UNIQUE NOT NULL,
 		class_register_notes_id int NOT NULL, -- НО НЕ UNIQUE! Значит, может быть больше одного коммментария
 
 		CONSTRAINT FK_class_register_notes_comment_id FOREIGN KEY (comment_id) REFERENCES comments (id),
 		CONSTRAINT FK_class_register_note_id FOREIGN KEY (class_register_notes_id) REFERENCES class_register_notes (id),
 	);
+	-- Получается для каждой записи (class_register_note) можно оставлять сколько угодно коментариев
 
-	--#################################################################################################################
+--#############################################################################################################################################################################
+
+
+ --4 (дополнительно) Добавить возможность преподавателю создавать ДЗ для каждой пары
+ --   - тема
+ --   - файл с заданием
+ --   - описание
+ --   - установка крайней даты сдачи ДЗ
+
+ CREATE TABLE file_paths (
+	id int PRIMARY KEY IDENTITY(1, 1),
+	file_path nvarchar(500),
+ );
+
+ CREATE TABLE homeworks (
+	id int PRIMARY KEY IDENTITY(1, 1),
+	pair_id int NOT NULL,
+	task_file_path int NOT NULL,
+	description nvarchar(MAX) NULL,
+	deadline datetime2 NOT NULL,
+
+	CONSTRAINT FK_homework_topic FOREIGN KEY (pair_id) REFERENCES pairs(id),
+	CONSTRAINT FK_task_file_path FOREIGN KEY (task_file_path) REFERENCES file_paths(id),
+ );
+
+
+ --#############################################################################################################################################################################
+
+ --5 (дополнительно) Добавить возможность студенту загружать ДЗ (с возможностью пересдачи)
+ --   - файл с решением
+ --   - комментарий
+ --   - дата загрузки
+
+CREATE TABLE homework_submissions (
+	id int PRIMARY KEY IDENTITY(1, 1),
+	homework_id int NOT NULL,
+	student_id int NOT NULL,
+	file_path_id int NOT NULL,      -- Файл с решением
+	student_comment_id int NULL,
+	submission_date datetime2 NOT NULL DEFAULT GETDATE(),
+
+	CONSTRAINT FK_submissions_homework FOREIGN KEY (homework_id) REFERENCES homeworks(id),
+    CONSTRAINT FK_submissions_student FOREIGN KEY (student_id) REFERENCES student_profiles(id), -- Предполагается наличие таблицы students
+    CONSTRAINT FK_submissions_file FOREIGN KEY (file_path_id) REFERENCES file_paths(id),
+	CONSTRAINT FK_student_comment FOREIGN KEY (student_comment_id) REFERENCES comments(id),
+);
+
+--#############################################################################################################################################################################
+
+--6 (дополнительно) Добавить возможность преподавателю оценивать загруженное студентом ДЗ
+
+CREATE TABLE homework_grades (
+	id int PRIMARY KEY IDENTITY(1, 1),
+	submission_id int NOT NULL UNIQUE, -- Оценка привязана к конкретной попытке
+	teacher_id int NOT NULL,
+	grade_id int NOT NULL,
+	teacher_comment_id int NULL,
+	graded_at datetime2 NOT NULL DEFAULT GETDATE(),
+
+	CONSTRAINT FK_grades_submission FOREIGN KEY (submission_id) REFERENCES homework_submissions(id),
+    CONSTRAINT FK_grades_teacher FOREIGN KEY (teacher_id) REFERENCES employee_profiles(id), 
+    CONSTRAINT FK_grades_value FOREIGN KEY (grade_id) REFERENCES grades(id),
+	CONSTRAINT FK_teacher_comment FOREIGN KEY (teacher_comment_id) REFERENCES comments(id), 
+);
+
+
+
+
+
+
+
 
